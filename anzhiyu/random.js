39ff08aca7f3b2a7982091acfc8cfad3415e3e4c
jatopos <843165112@qq.com>
2025-10-05 23:33:24 +0800

    Site updated: 2025-10-05 23:33:23

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/09/25/IMJATOPOS/","2025/10/05/My-New-Post/","2025/09/25/hello-world/","2025/10/05/文件上传/","2025/10/05/N1CTF Junior 2025 2_2/"];function toRandomPost(){
+var posts=["2025/09/25/IMJATOPOS/","2025/09/25/hello-world/","2025/10/05/文件上传/","2025/10/05/N1CTF Junior 2025 2_2/","2025/10/05/海口风景/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
